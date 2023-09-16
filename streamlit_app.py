18ac16f4d63883151c661efcb478a086da72f2ab
Joseph Moussa <73402811+joseph-moussa@users.noreply.github.com>
2023-09-16 12:02:13 +0200

    Added data folder to path

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,14 +6,7 @@ import seaborn as sns
 import matplotlib.pyplot as plt
 
 # Data can be downloaded from the links in the readme file
-data = pd.read_parquet("./train.parquet")
-'''
-data_url = "https://github.com/ramp-kits/bike_counters/releases/download/v0.1.0/train.parquet"
-response = requests.get(data_url)
-parquet_content = response.content
-parquet_io = BytesIO(parquet_content)
-data = pd.read_parquet(parquet_io)
-'''
+data = pd.read_parquet("data/train.parquet")
 
 # Function to visualize data on a map
 def visualize_data_on_map(data):
